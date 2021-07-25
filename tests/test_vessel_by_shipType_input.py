eb1c1c3a1361362fc02f10110ba6b9888f4ea87a
Bruce Bookman <78374623+brucebookman@users.noreply.github.com>
2021-07-24 18:23:06 -0700

    shipType now tested

--- tests/test_vessel_by_shipType_input.py
+++ tests/test_vessel_by_shipType_input.py
@@ -2,54 +2,77 @@ from pytest_bdd import scenario, when, then
 import pytest
 from nested_lookup import nested_lookup as nl
 from helpers import get_query
-from loguru import logger
 import pytest_check as check
+from loguru import logger
 
-DATA: list = list()
 
-
-@pytest.mark.long
+@pytest.mark.short
 @pytest.mark.positive_test
-@scenario(scenario_name='Execute a query for one or more shipType(s)',
+@pytest.mark.smoke_test
+@scenario(scenario_name='Single request for each shipType',
           feature_name="vessel_by_shipType_input.feature")
-def test_ship_type_input():
+def test_single_type():
     pass
 
+@pytest.mark.short
+@pytest.mark.positive_test
+@pytest.mark.smoke_test
+@scenario(scenario_name='More than one shipType is specified as input',
+          feature_name="vessel_by_shipType_input.feature")
+def test_multi_types():
+    pass
+
+@pytest.fixture
+@when('a "<shipType>" is specified for query input')
+def get_single_shipType_response(full_auth_client, shipType):
+    response: dict
+    client = full_auth_client
+    input_text: str = f'(shipType: "{shipType}" _limit: 1000)'
+    try:
+        response = client.execute(get_query(input_text))
+    except BaseException as e:
+        logger.error(e)
+        raise
+    return response, shipType
+
+
+@then("the response will contain only that shipType")
+def validate_ship_type(get_single_shipType_response):
+    response: dict
+    shipType: str
+    response, shipType = get_single_shipType_response
+    shipTypes: list = nl('shipType', response)
+
+    for s_type in shipTypes:
+        if s_type != shipType:
+            logger.error(f"LOOKING FOR {shipType}")
+            logger.error(f"FOUND THIS ONE INSTEAD: {s_type}\n")
+            logger.error(response)
+        check.is_true(s_type == shipType)
 
 
 @pytest.fixture
-@when("one or more shipType(s) are specified for input")
-def ship_type_input(full_auth_client):
-    return full_auth_client
+@when('"<shipTypes>" are specified')
+def get_multi_shipTypes_response(full_auth_client, shipTypes):
+    response: dict
+    ship_list: list = shipTypes.split()
+    string = ''
+    for ship in ship_list:
+        string+= f'"{ship}",'
+    input_text: str = f'(shipType: [{string}] _limit: 1000)'
+    try:
+        response = full_auth_client.execute(get_query(input_text))
+    except BaseException as e:
+        logger.error(e)
+        raise
+    return response, ship_list
 
 
-@then("the results will include the shipType specified and no other")
-def validate(ship_type_input):
-    global DATA
-    client = ship_type_input
-    # valid ship types
-    shipType_input: str
-    for i in range(0, 100):
-        if i <= 9:
-            shipType_input = f"0{i}"
-        else:
-            shipType_input = f"{i}"
-        input_text = f"""(shipType: ["{shipType_input}"]
-        _limit: 1000)"""
-        v2 = client.execute(get_query(input_text=input_text))
-        v2_vessels: list = nl('vessel', v2)
-        for vessel in v2_vessels:
-            pretty: str = str()
-            for k, v in vessel.items():
-                if v:
-                    pretty += f'{k}:{v}\n'
-            shipType_response = vessel['shipType']
-            check.is_true(shipType_response)
-            if not shipType_response == shipType_input:
-                logger.error(f"""
-                shipType_input: {shipType_input}
-                vessel: 
-                
-                {pretty}
-                """)
-            check.is_in(shipType_response, shipType_input)
+@then("the response will contain only those shipType(s)")
+def validate_ship_types(get_multi_shipTypes_response):
+    response: dict
+    ship_list: list
+    response, ship_list = get_multi_shipTypes_response
+    result_shipTypes: list = nl('shipType', response)
+    for s_type in result_shipTypes:
+        check.is_in(s_type, ship_list)
--- tests/vessel_by_shipType_input.feature
+++ tests/vessel_by_shipType_input.feature
@@ -1,27 +1,57 @@
-# Created by brucebookman at 6/4/21
-Feature: Ability to obtain static vessel data for specified shipType(s)
-  From PRD:
-    - The gQL endpoint allows filtering by Vessel Type(s)
+# Created by brucebookman at 7/24/21
+Feature: Restrict results by shipType
+  Reference shiptypes: https://docs.google.com/spreadsheets/d/1H5kgLS3uFaS269eerIocUsa-7S904tQM9ZnkKUjoqAQ/edit#gid=520822420
 
-  Info:
-  https://www.navcen.uscg.gov/?pageName=AISMessagesB
-    0 = not available or no ship = default
-    1-99 = as defined
-    100-199 = reserved, for regional use
-    200-255 = reserved, for future use
-
-  Test:
-      In Scope:
-        - All ship types are input (code only)
-
-      Out of Scope:
-        - if any optional data returned is null, it is ignored
-        - compare to v1 results MMSIs
+  In Scope:
+    - Every shipType as singular input
+    - Various shipType(s) as ORed input, see the Scenario Outline Examples
+    - Only 1000 records are inspected, no paging
+  Out of Scope:
+    - Data verification
+    - compare to v1
 
   Background:
     Given an authenticated gql client with full access
 
-  @positive_test @long @smoke_test
-  Scenario: Execute a query for one or more shipType(s)
-    When one or more shipType(s) are specified for input
-    Then the results will include the shipType specified and no other
+  Scenario Outline: Single request for each shipType
+    When a "<shipType>" is specified for query input
+    Then the response will contain only that shipType
+    Examples:
+    |shipType                   |
+    |Others                     |
+    |Fishing                    |
+    |Tug                        |
+    |Dredger                    |
+    |Offshore                   |
+    |Military ops               |
+    |Sailing                    |
+    |Yacht                      |
+    |Pilot Vessel               |
+    |Search and Rescue          |
+    |Law Enforcement            |
+    |Passenger                  |
+    |Bulk Carrier               |
+    |Tanker                     |
+    |Dry Bulk                   |
+    |Combination Carrier        |
+    |Containers                 |
+    |General Cargo              |
+    |Tankers-Chemicals          |
+    |General Tanker             |
+    |LNG Carriers               |
+    |Gas Carriers               |
+    |Tankers - Product Tankers  |
+    |Tankers - Crude            |
+    |Car Carrier                |
+    |Roll-on Roll-off           |
+    |Vehicle/Passenger          |
+    |Reefer                     |
+    |Livestock                  |
+
+    Scenario Outline: More than one shipType is specified as input
+      When "<shipTypes>" are specified
+      Then the response will contain only those shipType(s)
+      Examples:
+      |shipTypes                                          |
+      |Fishing Tanker Yacht                               |
+      |Containers Sailing Passenger Tug                   |
