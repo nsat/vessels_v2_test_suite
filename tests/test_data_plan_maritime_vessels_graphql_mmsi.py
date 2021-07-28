b98c78bc6c8ce1d07f4822a7a0466a60cda582a4
Bruce Bookman <78374623+brucebookman@users.noreply.github.com>
2021-07-28 14:55:11 -0700

    updated data plan mmsi test

--- tests/test_data_plan_maritime_vessels_graphql_mmsi.py
+++ tests/test_data_plan_maritime_vessels_graphql_mmsi.py
@@ -1,9 +1,9 @@
 from pytest_bdd import scenario, when, then
 import pytest
 from nested_lookup import nested_lookup as nl
-from helpers import get_query
 import pytest_check as check
 from loguru import logger
+from query_sets import query_sets
 
 
 @pytest.mark.positive_test
@@ -26,9 +26,10 @@ def get_vars(mmsi):
 def verify_filtering(get_client, get_vars):
     client = get_client
     mmsi_inputs: list = get_vars
-
+    qs = query_sets.GetQuery()
+    gql_query = qs.get_vessels_gql_query()
     # a generic query should only include mmsi that are authorized
-    response = client.execute(get_query())
+    response = client.execute(gql_query)
     mmsi_outputs = nl('mmsi', response)
     for mmsi in mmsi_outputs:
         if not str(mmsi) in mmsi_inputs:
